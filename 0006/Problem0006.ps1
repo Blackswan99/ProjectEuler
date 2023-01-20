12013e13978eab759a1a756f279a3633dbd9a813
Blackswan99 <37673853+Blackswan99@users.noreply.github.com>
2023-01-20 13:00:25 +0100

    Update Problem0006.ps1

--- 0006/Problem0006.ps1
+++ 0006/Problem0006.ps1
@@ -1,29 +1,30 @@
-#The sum of the squares of the first ten natural numbers is 385,
-#The square of the sum of the first ten natural numbers is 3025,
-#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025-385=2640
-#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+#include <iostream>
 
-function SumOfSqares {
-    param(
-        [int64] $UpTo
-    )
-    $sum = 0
-    for ($i = 1; $i -lt $UpTo + 1; $i++){
-        $sum += $i*$i
+//The sum of the squares of the first ten natural numbers is 385,
+//The square of the sum of the first ten natural numbers is 3025,
+//Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025-385=2640
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+long int sum_of_squares (int upto) {
+
+    long int sum {0};
+    for (int i = 1; i < upto + 1; i++){
+        sum += i*i;
     }
-    return $sum
+    return sum;
 }
 
-function SqareOfSum {
-    param(
-        [int64] $UpTo
-    )
-    $sum = 0
-    for ($i = 1; $i -lt $UpTo + 1; $i++){
-        $sum += $i
+long int sqares_of_sum (int upto){
+
+    long int sum {0};
+    for (int i = 1; i < upto + 1; i++){
+        sum += i;
     }
-    return $sum*$sum
+    return sum*sum;
 }
 
-Write-Host "The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is: " -NoNewline
-Write-Host $((SqareOfSum -UpTo 100)-(SumOfSqares -UpTo 100))
+int main(){
+    std::cout << "The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is: ";
+    std::cout << sqares_of_sum(100)- sum_of_squares(100);
+    return 0;
+}
